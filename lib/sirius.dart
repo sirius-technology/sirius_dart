d973799af229339141e38947a81ec9e024090bcb
somesh sahu <sahusomesh77@gmail.com>
2025-02-14 12:07:14 +0530

    model export

--- lib/sirius.dart
+++ lib/sirius.dart
@@ -4,5 +4,6 @@ library;
 export 'src/server.dart';
 export 'src/response.dart';
 export 'src/request.dart';
+export 'src/model.dart';
 
 // TODO: Export any libraries intended for clients of this package.
