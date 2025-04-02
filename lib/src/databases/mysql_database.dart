a0d39838fb20ba3aa458f70ce3fa01cb137fba2a
somesh sahu <sahusomesh77@gmail.com>
2025-04-02 11:05:29 +0530

    fixes and publishing

--- lib/src/databases/mysql_database.dart
+++ lib/src/databases/mysql_database.dart
@@ -1,18 +1,18 @@
-import 'package:mysql1/mysql1.dart';
-import 'package:sirius/src/databases/database_config.dart';
+// import 'package:mysql1/mysql1.dart';
+// import 'package:sirius/src/databases/database_config.dart';
 
-final ConnectionSettings _mySqlSetting = ConnectionSettings(
-  host: DatabaseConfig.host,
-  port: DatabaseConfig.port,
-  user: DatabaseConfig.user,
-  password: DatabaseConfig.password,
-  db: DatabaseConfig.database,
-);
+// final ConnectionSettings _mySqlSetting = ConnectionSettings(
+//   host: DatabaseConfig.host,
+//   port: DatabaseConfig.port,
+//   user: DatabaseConfig.user,
+//   password: DatabaseConfig.password,
+//   db: DatabaseConfig.database,
+// );
 
-Future<MySqlConnection> connectMySql() async {
-  return await MySqlConnection.connect(_mySqlSetting);
-}
-
-// Future<void> closeMySql(MySqlConnection conn) async {
-//   await conn.close();
+// Future<MySqlConnection> connectMySql() async {
+//   return await MySqlConnection.connect(_mySqlSetting);
 // }
+
+// // Future<void> closeMySql(MySqlConnection conn) async {
+// //   await conn.close();
+// // }
